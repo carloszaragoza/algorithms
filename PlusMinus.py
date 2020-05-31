d3180d229e965206dfaddf76c1932f07d3742414
Carlos Zaragoza <carast@gmail.com>
2020-05-31 15:21:53 -0400

    Diagonal Differences

---
+++ PlusMinus.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+import os
+import sys
+
+def plusMinus(arr):
+    # Solution
+    if len(arr) >= -100 and len(arr) <= 100:
+        diagonal_one = 0
+        diagonal_two = 0
+
+        for x in range(len(arr)):
+            nextList = [item[x] for item in arr]
+            diagonal_one += nextList[x]
+            diagonal_two += nextList[-(x+1)]
+    return abs(diagonal_one - diagonal_two)
+
+if __name__ == '__main__':
+
+    diagonalMatrix = [[11, 2, 4], [4, 5, 6], [10, 8, -12]]
+
+    result = plusMinus(diagonalMatrix)
+
+    print(str(result) + '\n')
