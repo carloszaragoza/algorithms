76d4c7c3868147d93565cb23b7ec991dd55b4ae4
Carlos Zaragoza <carast@gmail.com>
2020-06-02 00:16:44 -0400

    Plus Minus problem

--- PlusMinus.py
+++ PlusMinus.py
@@ -3,22 +3,30 @@
 import os
 import sys
 
+
 def plusMinus(arr):
     # Solution
-    if len(arr) >= -100 and len(arr) <= 100:
-        diagonal_one = 0
-        diagonal_two = 0
+    positive = 0
+    negative = 0
+    zero = 0
+    size = len(arr)
+    if size > 0 and size <= 100:
+        for x in range(size):
+            if arr[x] > 0:
+                positive += 1
+            if arr[x] < 0:
+                negative += 1
+            if arr[x] == 0:
+                zero += 1
+    positive = float("{:.6f}".format(positive/size))
+    negative = float("{:.6f}".format(negative/size))
+    zero = float("{:.6f}".format(zero/size))
 
-        for x in range(len(arr)):
-            nextList = [item[x] for item in arr]
-            diagonal_one += nextList[x]
-            diagonal_two += nextList[-(x+1)]
-    return abs(diagonal_one - diagonal_two)
+    print(str(positive) + '\n' + str(negative) + '\n' + str(zero))
+
 
 if __name__ == '__main__':
 
-    diagonalMatrix = [[11, 2, 4], [4, 5, 6], [10, 8, -12]]
+    matrix = [-4, 3, -9, 0, 4, 1]
 
-    result = plusMinus(diagonalMatrix)
-
-    print(str(result) + '\n')
+    result = plusMinus(matrix)
